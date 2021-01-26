2920e5952d0e1fa8aaa306ca042382eaa8aeee03
莫盛 <929262660@qq.com>
2021-01-26 22:04:40 +0800

    日期工具类

--- src/main/java/utils/DateUtil.java
+++ src/main/java/utils/DateUtil.java
@@ -6,37 +6,39 @@ import java.time.temporal.ChronoUnit;
 
 public class DateUtil {
     /**
+     * 调整时间
+     * 当需要返回月份时pattern参数传入MM同理。。。
      * @param offset:1表示明天，-1表示昨天
      * @return
      */
-    public LocalDateTime modifyDayDate(int offset) {
-        return currentDate().plusDays(offset);
+    public static String modifyDate(int offset) {
+        return modifyDate(ChronoUnit.DAYS, offset, "yyyy-MM-dd HH:mm:ss");
     }
 
-    //调整时间
-    public LocalDateTime modifyMonthDate(int offset) {
-        return currentDate().plus(offset, ChronoUnit.MONTHS);
+    public static String modifyDate(ChronoUnit chronoUnit, int offset, String pattern) {
+        return formatDate(LocalDateTime.now().plus(offset, chronoUnit), pattern);
     }
 
-    public LocalDateTime currentDate() {
-        return LocalDateTime.now();
+    public static String currentDate() {
+        return currentDate("yyyy-MM-dd HH:mm:ss");
     }
 
-    public String formatDate(LocalDateTime date) {
-        return formatDate(date,"yyyy-MM-dd HH:mm:ss");
+    public static String currentDate(String pattern) {
+        return formatDate(LocalDateTime.now(), pattern);
     }
-    public String formatDate(LocalDateTime date, String pattern) {
+
+    public static String formatDate(LocalDateTime date, String pattern) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern);
         return dtf.format(date);
     }
 
     //当前时间戳
-    public String nowTimeLong() {
+    public static String nowTimeLong() {
         return Long.toString(System.currentTimeMillis());
     }
 
     //指定时间戳
-    public String timeLongByDate(String dateTime) {
+    public static String timeLongByDate(String dateTime) {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate date = LocalDate.parse(dateTime, fmt);
         long time = date.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
